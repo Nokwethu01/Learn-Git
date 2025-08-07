c07707678371d648f5564b82ff27633049ed8903
Nokwethu01 <kwethu259@gmail.com>
2025-08-07 13:14:10 +0200

    jjpw

--- main.py
+++ main.py
@@ -6,7 +6,20 @@ def add_numbers(a, b):
     """Returns the sum of two numbers."""
     return a + b
 
+def main():
+    user_name = input("Enter your name: ")
+    print(greet_user(user_name))
+
+    try:
+        x = float(input("Enter the first number: "))
+        y = float(input("Enter the second number: "))
+        result = add_numbers(x, y)
+        print(f"The sum of {x} and {y} is {result}.")
+    except ValueError:
+        print("Please enter valid numbers.")
+        
 
 
 
 
+print("that was a huge mistake but I have my files back")
